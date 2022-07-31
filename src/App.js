09a4564dcd17c48dcfb77d36f165b81c0af5da7f
Jarred Mac Intosh <macintosh@rvc.nl>
2022-08-01 01:24:26 +0200

    Images can be opend in a modal.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
 const axios = require('axios').default;
 
 class App extends React.Component {
   endPointUrl = 'https://api.unsplash.com';
+  
 
   constructor(props) {
     super(props);
     this.state = {
-      images: []
+      images: [],
+      open: false,
+      imageModelIndex: 0
     }
   }
 
@@ -36,12 +41,50 @@ class App extends React.Component {
       <div className="container mx-auto">
         <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {
-            images.map(image => <div key={image.id} className=""><img key={image.id} src={image.urls.small} alt="Loading..."></img></div>)
+            images.map((image, index) => 
+              <div key={image.id} className="">
+                <img key={image.id} src={image.urls.small} alt="Loading..." onClick={() => this.setState({open: true, imageModelIndex: index})}></img>
+              </div>
+            )
           }
         </div>
+        <ImageModal
+          data={this.state.images[this.state.imageModelIndex]}
+          show={this.state.open}
+          onHide={() => this.setState({open: false})}
+        />
       </div>
     );
   };
+  
+}
+
+function ImageModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props?.data?.location.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <img src={props?.data?.urls?.full} alt="Loading..."></img>
+      </Modal.Body>
+      <Modal.Footer>
+        <div className='flex flex-col'>
+          <div>Height: {props?.data?.height}</div>
+          <div>Width: {props?.data?.width}</div>
+          <div>Make: {props?.data?.exif?.make}</div>
+          <div>Model: {props?.data?.exif?.model}</div>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default App;
