ef3fff5f96c2f4e6ec77b48e9bcd62e44759853b
Jarred Mac Intosh <macintosh@rvc.nl>
2022-07-31 15:44:02 +0200

    Added axios for http requests. Used the photos random endpoint from unsplash to get nine photos.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
+import React from 'react';
+const axios = require('axios').default;
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  );
+class App extends React.Component {
+  endPointUrl = 'https://api.unsplash.com';
+
+  componentDidMount() {
+    axios.get('/photos/random', {
+      baseURL: this.endPointUrl,
+      headers: {
+        'Accept-Version': 'v1',
+        'Authorization': 'Client-ID h_uHNthSMyHx38lq4J-KHrHvF2LjLz2bWWjzEQRTzO0'
+      },
+      params: {
+        count: 9
+      }
+    })
+    .then(response => {
+      console.log(response);
+    })
+  }
+
+  render() {
+    return(
+      <div className="container mx-auto">
+        <div className="flex flex-row">
+          <div className="basis-1/3">01</div>
+          <div className="basis-1/3">02</div>
+          <div className="basis-1/3">03</div>
+        </div>
+        <div className="flex flex-row">
+          <div className="basis-1/3">01</div>
+          <div className="basis-1/3">02</div>
+          <div className="basis-1/3">03</div>
+        </div>
+        <div className="flex flex-row">
+          <div className="basis-1/3">01</div>
+          <div className="basis-1/3">02</div>
+          <div className="basis-1/3">03</div>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
